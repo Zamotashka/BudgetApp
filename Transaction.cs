a66f654633cc438172bc0fef3d40f6937467a340
Proskurov <proskurov@example.com>
2026-04-08 22:46:00 +0300

    Добавлен код Transaction.cs

--- Transaction.cs
+++ Transaction.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BudgetApp
 {
-    internal class Transaction
+    public enum TransactionType
     {
+        Доход,
+        Расход
+    }
+
+    public class Transaction
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction(string description, decimal amount, TransactionType type, DateTime date)
+        {
+            Description = description;
+            Amount = amount;
+            Type = type;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            string typeStr = Type == TransactionType.Доход ? "Доход" : "Расход";
+            return $"{Description} - {typeStr} ({Amount} руб.) [{Date:dd.MM.yyyy}]";
+        }
     }
 }
